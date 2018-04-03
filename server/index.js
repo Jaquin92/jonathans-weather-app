a290ae21d297bc65f0e712df8e4df40bbbb43223
Jonathan <jaquin92@yahoo.com>
2018-04-02 22:28:27 -0500

    done

--- server/index.js
+++ server/index.js
@@ -6,9 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const { getWeather } = require("./controller");
+app.use(express.static(`${__dirname}/../build`));
 
-app.get("/api/weather/:id", getWeather);
+const path = require('path')
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
 
 const port = 3002;
 
